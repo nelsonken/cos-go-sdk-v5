ed6aa5f80b5c1be8553635c7cc8e4a7baf3b534a
ken <31346910+nelsonken@users.noreply.github.com>
2018-03-30 20:57:10 +0800

    Update setup_test.go

--- cos/setup_test.go
+++ cos/setup_test.go
@@ -12,7 +12,7 @@ const (
 )
 
 func setUp() {
-	client = New(&Option{"1254217795", "AKIDkOq6C6qLgstiMqmGF2d3HKBpHYeZlpAH", "Rny65tVv9BQuHUVKxOehZFqJbifYN7g3", "ap-chengdu", "myqcloud.com", "omnipay-entity-dev"})
+	client = New(&Option{"", "", "", "", "", ""})
 
 }
 
